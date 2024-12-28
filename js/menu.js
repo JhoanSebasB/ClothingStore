804534fec98a232f767c9b5971b11dbac83beb51
Jhoan Sebastian <143830061+JhoanSebasB@users.noreply.github.com>
2024-12-28 16:07:14 -0500

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -9,3 +9,18 @@ openMenu.addEventListener("click", () => {
 closeMenu.addEventListener("click", () => {
     aside.classList.remove("aside-visible");
 })
+
+/* Carrusel informativo */
+const messages = ["Envio gratis con compras superiores a $199.900", "Devoluciones Gratis ", "Envio gratis con compras superiores a $199.900", "Devoluciones Gratis "];
+let index = 0;
+
+const carouselText = document.getElementById("carouselText");
+
+function updateText() {
+    carouselText.textContent = messages[index];
+    index = (index + 1) % messages.length; // Cicla los mensajes
+}
+
+// Inicializa el texto y actualiza cada 8 segundos
+updateText();
+setInterval(updateText, 5000); // Sincronizado con la animación
