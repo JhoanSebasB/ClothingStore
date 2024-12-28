0af2b65fa5666b3a4311f074e7070ccb224bc844
Jhoan Sebastian <143830061+JhoanSebasB@users.noreply.github.com>
2024-12-28 17:02:26 -0500

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -10,6 +10,8 @@ closeMenu.addEventListener("click", () => {
     aside.classList.remove("aside-visible");
 })
 
+
+
 /* Carrusel informativo */
 const messages = ["Envio gratis con compras superiores a $199.900", "Devoluciones Gratis ", "Envio gratis con compras superiores a $199.900", "Devoluciones Gratis "];
 let index = 0;
@@ -18,9 +20,31 @@ const carouselText = document.getElementById("carouselText");
 
 function updateText() {
     carouselText.textContent = messages[index];
-    index = (index + 1) % messages.length; // Cicla los mensajes
+    index = (index + 1) % messages.length; 
 }
 
-// Inicializa el texto y actualiza cada 8 segundos
+
 updateText();
-setInterval(updateText, 5000); // Sincronizado con la animación
+setInterval(updateText, 5000);
+
+
+
+/* Cards */
+
+const buttons = document.querySelectorAll('.btn');
+const cards = document.querySelectorAll('.card');
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const filter = button.getAttribute('data-filter');
+
+        cards.forEach(card => {
+            if (filter === 'all' || card.getAttribute('data-category') === filter) {
+                card.classList.remove('hidden'); // Muestra las tarjetas
+            } else {
+                card.classList.add('hidden'); // Oculta las tarjetas
+            }
+        });
+    });
+});
+
