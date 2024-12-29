acf0599547f771873d6a068b1b1ef7d13e3aa2a2
Jhoan Sebastian <143830061+JhoanSebasB@users.noreply.github.com>
2024-12-29 18:40:59 -0500

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -48,3 +48,87 @@ buttons.forEach(button => {
     });
 });
 
+
+
+/* Cambiar imagenes en las cards */ 
+
+
+const shoeColors = [
+    // Negro
+    [
+        "https://static.nike.com/a/images/w_1280,q_auto,f_auto/8636ce95-74a9-40b5-b83a-2d095d8faac2/dunk-low-black-release-date.jpg",
+        "https://static.nike.com/a/images/w_1280,q_auto,f_auto/a3e7dead-1ad2-4c40-996d-93ebc9df0fca/dunk-low-black-release-date.jpg",
+        "https://static.nike.com/a/images/w_1280,q_auto,f_auto/810eecdd-4534-4846-85c5-b977d9284d0e/dunk-low-black-release-date.jpg"
+    ],
+    // Azul
+    [
+        "https://static.nike.com/a/images/w_1280,q_auto,f_auto/b7989758-77dd-43e0-b5ad-5377f9574aa5/dunk-low-university-blue-release-date.jpg",
+        "https://static.nike.com/a/images/w_1280,q_auto,f_auto/63c770a7-fc62-44e8-a7ba-cd47d130d448/dunk-low-university-blue-release-date.jpg",
+        "https://static.nike.com/a/images/w_1280,q_auto,f_auto/d5b698f4-3351-4d97-a054-f88c1eeb7379/dunk-low-university-blue-release-date.jpg"
+    ],
+    // Blanco
+    [
+        "https://i.imgur.com/4xVwJGy.png",
+        "https://i.imgur.com/sfBChVm.png",
+        "https://i.imgur.com/QJ9Y48C.png"
+    ]
+];
+
+let currentColorIndexHombre = 0; // Índice del color actual para hombre
+let currentImageIndexHombre = 0; // Índice de la imagen actual para hombre
+
+let currentColorIndexMujer = 0; // Índice del color actual para mujer
+let currentImageIndexMujer = 0; // Índice de la imagen actual para mujer
+
+// Cambiar imagen dentro del mismo color
+function changeCardImage(direction, imageId) {
+    const imgElement = document.getElementById(imageId);
+    const images = imageId.includes('hombre') ? shoeColors[currentColorIndexHombre] : shoeColors[currentColorIndexMujer];
+
+    if (direction === 'prev') {
+        if (imageId.includes('hombre')) {
+            currentImageIndexHombre = (currentImageIndexHombre - 1 + images.length) % images.length;
+        } else {
+            currentImageIndexMujer = (currentImageIndexMujer - 1 + images.length) % images.length;
+        }
+    } else if (direction === 'next') {
+        if (imageId.includes('hombre')) {
+            currentImageIndexHombre = (currentImageIndexHombre + 1) % images.length;
+        } else {
+            currentImageIndexMujer = (currentImageIndexMujer + 1) % images.length;
+        }
+    }
+
+    imgElement.src = images[imageId.includes('hombre') ? currentImageIndexHombre : currentImageIndexMujer];
+}
+
+// Cambiar el color del zapato (y reiniciar el índice de la imagen)
+function changeShoeColor(colorIndex, imageId) {
+    if (imageId.includes('hombre')) {
+        currentColorIndexHombre = colorIndex;
+        currentImageIndexHombre = 0;
+    } else {
+        currentColorIndexMujer = colorIndex;
+        currentImageIndexMujer = 0;
+    }
+
+    const imgElement = document.getElementById(imageId);
+    const images = imageId.includes('hombre') ? shoeColors[currentColorIndexHombre] : shoeColors[currentColorIndexMujer];
+    imgElement.src = images[0];
+}
+
+// Asegurarse de que las imágenes se carguen correctamente al recargar la página
+window.onload = function() {
+    const imgElementHombre = document.getElementById('card-image-1-hombre');
+    const imgElementMujer = document.getElementById('card-image-1-mujer');
+    imgElementHombre.src = shoeColors[currentColorIndexHombre][currentImageIndexHombre]; // Inicializa la imagen de hombre
+    imgElementMujer.src = shoeColors[currentColorIndexMujer][currentImageIndexMujer]; // Inicializa la imagen de mujer
+}
+
+
+/* COOKIES */
+
+/* Menu Scroll */
+
+
+        
