ff75a3ce5147d4577cb569afdccc31f2814c0804
Jhoan Sebastian <143830061+JhoanSebasB@users.noreply.github.com>
2025-01-18 11:48:31 -0500

    Update image.js

--- js/image.js
+++ js/image.js
@@ -25,6 +25,18 @@ const shoeColors = [
         "https://nikeco.vtexassets.com/arquivos/ids/628035-1280-auto?v=638550347320600000&width=150&height=auto&aspect=true",
         "https://nikeco.vtexassets.com/arquivos/ids/614796-1280-auto?v=638549409135100000&width=150&height=auto&aspect=true",
         "https://nikeco.vtexassets.com/arquivos/ids/621614-1280-auto?v=638550341020470000&width=150&height=auto&aspect=true"
+    ],
+    // green
+    [
+        "https://static.nike.com/a/images/w_1280,q_auto,f_auto/2060d165-a7df-4792-90b3-1fab22c4d53e/fecha-de-lanzamiento-de-los-dunk-low-varsity-green-dd1391-101.jpg",
+        "https://static.nike.com/a/images/w_1280,q_auto,f_auto/f97ac93f-76da-4861-b00a-6d75555629a1/fecha-de-lanzamiento-de-los-dunk-low-varsity-green-dd1391-101.jpg",
+        "https://static.nike.com/a/images/w_1280,q_auto,f_auto/220d9864-d99e-488e-8fd8-54548e6d2a4b/fecha-de-lanzamiento-de-los-dunk-low-varsity-green-dd1391-101.jpg"
+    ],
+     // Dusty Olive
+     [
+        "https://static.nike.com/a/images/w_1280,q_auto,f_auto/0dfd22b1-ec58-4475-8487-6950f5947f8b/fecha-de-lanzamiento-del-dunk-low-dusty-olive-dh5360-300.jpg",
+        "https://static.nike.com/a/images/w_1280,q_auto,f_auto/e4aa4acc-4d60-4a35-87fa-a180ea4626ef/fecha-de-lanzamiento-del-dunk-low-dusty-olive-dh5360-300.jpg",
+        "https://static.nike.com/a/images/w_1280,q_auto,f_auto/7adbf446-c4b1-454c-b865-ac5bcde64b93/fecha-de-lanzamiento-del-dunk-low-dusty-olive-dh5360-300.jpg"
     ]
 ];
 
@@ -41,21 +53,104 @@ const shoeairforce = [
         "https://static.nike.com/a/images/w_1280,q_auto,f_auto/ca38a642-ec6e-450f-8935-f0d71f279d33/air-force-1-07-fresh-dm0211-001.jpg",
         "https://static.nike.com/a/images/w_1280,q_auto,f_auto/a6b2de60-cc06-4757-88bb-5fc6556f816f/air-force-1-07-fresh-dm0211-001.jpg"
     ],
+    //Coconut milk
+    [
+        "https://static.nike.com/a/images/w_1280,q_auto,f_auto/956102de-72cf-4c3f-9855-d26d83474f0f/fecha-de-lanzamiento-del-air-force-1-07-coconut-milk-dm0211-101.jpg",
+        "https://static.nike.com/a/images/w_1280,q_auto,f_auto/ffd1fda2-bbbc-48a8-bd29-913a7be5cc09/fecha-de-lanzamiento-del-air-force-1-07-coconut-milk-dm0211-101.jpg",
+        "https://static.nike.com/a/images/w_1280,q_auto,f_auto/e400e54b-4b84-4ad9-9b37-f176fb8ecf2b/fecha-de-lanzamiento-del-air-force-1-07-coconut-milk-dm0211-101.jpg"
+    ],
+    //Oil Green
+    [
+        "https://static.nike.com/a/images/w_1280,q_auto,f_auto/c33cee88-cacd-43bd-9355-54a4be958594/fecha-de-lanzamiento-del-air-force-1-07-oil-green-fb8876-300.jpg",
+        "https://static.nike.com/a/images/w_1280,q_auto,f_auto/4435aebf-5273-48b7-83da-a8cdd491f811/fecha-de-lanzamiento-del-air-force-1-07-oil-green-fb8876-300.jpg",
+        "https://static.nike.com/a/images/w_1280,q_auto,f_auto/5b49c0e7-5708-4156-8b5f-8e0ac09472f0/fecha-de-lanzamiento-del-air-force-1-07-oil-green-fb8876-300.jpg"
+    ],
 ];
 
-const shoeNike = [
+const freemetconsix = [
     // Blanco
     [
         "https://nikeco.vtexassets.com/arquivos/ids/691041-1200-auto?v=638623774829370000&width=1200&height=auto&aspect=true",
         "https://nikeco.vtexassets.com/arquivos/ids/677983-1200-auto?v=638623624045400000&width=1200&height=auto&aspect=true",
         "https://nikeco.vtexassets.com/arquivos/ids/684610-1200-auto?v=638623759274430000&width=1200&height=auto&aspect=true"
     ],
-    // Verde
+    // Negros - blancos
     [
         "https://nikeco.vtexassets.com/arquivos/ids/704695-1200-auto?v=638628803305600000&width=1200&height=auto&aspect=true",
         "https://nikeco.vtexassets.com/arquivos/ids/702688-1200-auto?v=638628787023100000&width=1200&height=auto&aspect=true",
         "https://nikeco.vtexassets.com/arquivos/ids/703777-1200-auto?v=638628799300230000&width=1200&height=auto&aspect=true"
     ]
+];
+
+const airjordanlow = [
+    // Blanco
+    [
+        "https://nikeco.vtexassets.com/arquivos/ids/693135-1200-auto?v=638623776951400000&width=1200&height=auto&aspect=true",
+        "https://nikeco.vtexassets.com/arquivos/ids/678724-1200-auto?v=638623626098130000&width=1200&height=auto&aspect=true",
+        "https://nikeco.vtexassets.com/arquivos/ids/686014-1200-auto?v=638623761324000000&width=1200&height=auto&aspect=true"
+    ],
+    // Verde
+    [
+        "https://nikeco.vtexassets.com/arquivos/ids/693196-1200-auto?v=638623777015130000&width=1200&height=auto&aspect=true",
+        "https://nikeco.vtexassets.com/arquivos/ids/678746-1200-auto?v=638623626155870000&width=1200&height=auto&aspect=true",
+        "https://nikeco.vtexassets.com/arquivos/ids/686060-1200-auto?v=638623761387670000&width=1200&height=auto&aspect=true"
+    ],
+    // pink
+    [
+        "https://nikeco.vtexassets.com/arquivos/ids/693169-1200-auto?v=638623776988400000&width=1200&height=auto&aspect=true",
+        "https://nikeco.vtexassets.com/arquivos/ids/678735-1200-auto?v=638623626125070000&width=1200&height=auto&aspect=true",
+        "https://nikeco.vtexassets.com/arquivos/ids/686035-1200-auto?v=638623761353330000&width=1200&height=auto&aspect=true"
+    ],
+    // morado
+    [
+        "https://nikeco.vtexassets.com/arquivos/ids/633869-1200-auto?v=638550351938730000&width=1200&height=auto&aspect=true",
+        "https://nikeco.vtexassets.com/arquivos/ids/616101-1200-auto?v=638549412618670000&width=1200&height=auto&aspect=true",
+        "https://nikeco.vtexassets.com/arquivos/ids/625613-1200-auto?v=638550345218830000&width=1200&height=auto&aspect=true"
+    ],
+];
+const airjordanhighog = [
+    // black - red
+    [
+        "https://static.nike.com/a/images/w_1280,q_auto,f_auto/51426d43-2c8c-46ad-abcb-f77a4012e8c9/fecha-de-lanzamiento-del-air-jordan-1-high-og-satin-bred-para-mujer-fd4810-061.jpg",
+        "https://static.nike.com/a/images/w_1280,q_auto,f_auto/60d08fdd-d528-4305-994b-ab655665d769/fecha-de-lanzamiento-del-air-jordan-1-high-og-satin-bred-para-mujer-fd4810-061.jpg",
+        "https://static.nike.com/a/images/w_1280,q_auto,f_auto/7677bc69-2135-4e36-ac3b-6641a3ffe25e/fecha-de-lanzamiento-del-air-jordan-1-high-og-satin-bred-para-mujer-fd4810-061.jpg"
+    ],
+    // Blanco - negro
+    [
+        "https://static.nike.com/a/images/w_1280,q_auto,f_auto/0764eb6e-a137-4f7b-ba93-28dd7daa39c0/fecha-de-lanzamiento-del-air-jordan-1-high-og-black-white-dz5485-010.jpg",
+        "https://static.nike.com/a/images/w_1280,q_auto,f_auto/2b49243f-1c69-40e1-af12-5f35e37536cb/fecha-de-lanzamiento-del-air-jordan-1-high-og-black-white-dz5485-010.jpg",
+        "https://static.nike.com/a/images/w_1280,q_auto,f_auto/53eb5297-a087-40cc-aec0-d0eea51e2092/fecha-de-lanzamiento-del-air-jordan-1-high-og-black-white-dz5485-010.jpg"
+    ],
+    // blue
+    [
+        "https://static.nike.com/a/images/w_1280,q_auto,f_auto/88408d57-3d0f-4217-8eda-de4a6b628bd5/fecha-de-lanzamiento-del-air-jordan-1-high-university-blue-dz5485-400.jpg",
+        "https://static.nike.com/a/images/w_1280,q_auto,f_auto/ea7b8d93-0ec3-4497-b578-569e2b561a22/fecha-de-lanzamiento-del-air-jordan-1-high-university-blue-dz5485-400.jpg",
+        "https://static.nike.com/a/images/w_1280,q_auto,f_auto/ed2746e5-16b0-490a-a56f-a466cfe7ceb1/fecha-de-lanzamiento-del-air-jordan-1-high-university-blue-dz5485-400.jpg"
+    ],
+    // verde
+    [
+        "https://static.nike.com/a/images/w_1280,q_auto,f_auto/5da4b45b-3fcd-4892-90a2-b783425c8950/fecha-de-lanzamiento-del-air-jordan-1-high-black-and-lucky-green-dz5485-031.jpg",
+        "https://static.nike.com/a/images/w_1280,q_auto,f_auto/2156e314-9889-4bdc-962d-7350f66cdf7f/fecha-de-lanzamiento-del-air-jordan-1-high-black-and-lucky-green-dz5485-031.jpg",
+        "https://static.nike.com/a/images/w_1280,q_auto,f_auto/b70a0f07-ad8e-420c-aea3-71c8b43e55ff/fecha-de-lanzamiento-del-air-jordan-1-high-black-and-lucky-green-dz5485-031.jpg"
+    ],
+    // morado
+    [
+        "https://static.nike.com/a/images/w_1280,q_auto,f_auto/2c0f95d4-6400-40ef-9bc6-f5224c7f5460/fecha-de-lanzamiento-de-los-air-jordan-1-latte-para-mujer-fd2596-021.jpg",
+        "https://static.nike.com/a/images/w_1280,q_auto,f_auto/022c7053-5c55-4bc4-8cdc-72c6e8f95a5e/fecha-de-lanzamiento-de-los-air-jordan-1-latte-para-mujer-fd2596-021.jpg",
+        "https://static.nike.com/a/images/w_1280,q_auto,f_auto/60471aac-5614-4b8e-90db-f75611ca1c63/fecha-de-lanzamiento-de-los-air-jordan-1-latte-para-mujer-fd2596-021.jpg"
+    ],
+    // marron - negro
+    [
+        "https://static.nike.com/a/images/w_1280,q_auto,f_auto/5d13078c-28c4-4473-a207-ec93f3a3a8d9/fecha-de-lanzamiento-del-air-jordan-1-palomino-dz5485-020.jpg",
+        "https://static.nike.com/a/images/w_1280,q_auto,f_auto/6acedb46-7fa4-40e1-9bd2-dbb651e986a3/fecha-de-lanzamiento-del-air-jordan-1-palomino-dz5485-020.jpg",
+        "https://static.nike.com/a/images/w_1280,q_auto,f_auto/f2afb7d6-3fcf-4d18-adba-9728971577fe/fecha-de-lanzamiento-del-air-jordan-1-palomino-dz5485-020.jpg"
+    ],
+    // orange
+    [
+        "https://static.nike.com/a/images/w_1280,q_auto,f_auto/0119af08-f91d-46d5-bc9e-6bccd7527462/fecha-de-lanzamiento-del-air-jordan-1-high-og-yellow-ochre-dz5485-701.jpg",
+        "https://static.nike.com/a/images/w_1280,q_auto,f_auto/acf9cf41-a9bc-4e6a-b98a-7a38bb163f31/fecha-de-lanzamiento-del-air-jordan-1-high-og-yellow-ochre-dz5485-701.jpg",
+        "https://static.nike.com/a/images/w_1280,q_auto,f_auto/1f0f6e57-4cfa-4ff4-a08c-0052333c8548/fecha-de-lanzamiento-del-air-jordan-1-high-og-yellow-ochre-dz5485-701.jpgs"
+    ],
 ];
 
 let currentColorIndexHombre = 0; 
@@ -67,41 +162,59 @@ let currentImageIndexMujer = 0;
 let currentColorIndexNike = 0; 
 let currentImageIndexNike = 0;
 
+let currentColorIndexairjordanlow = 0; 
+let currentImageIndexairjordanlow = 0;
+
+let currentColorIndexairjordanhighog = 0; 
+let currentImageIndexairjordanhighog = 0;
+
 function changeCardImage(direction, imageId) {
     const imgElement = document.getElementById(imageId);
     let images;
+    let currentIndex;
 
-    // Cambia la lógica de selección de imágenes dependiendo de la id
+    // Definir las imágenes y los índices actuales según el tipo de imagen
     if (imageId.includes('hombre')) {
         images = shoeColors[currentColorIndexHombre];
+        currentIndex = currentImageIndexHombre;
     } else if (imageId.includes('mujer')) {
         images = shoeairforce[currentColorIndexMujer];
+        currentIndex = currentImageIndexMujer;
     } else if (imageId.includes('nike')) {
-        images = shoeNike[currentColorIndexNike];
+        images = freemetconsix[currentColorIndexNike];
+        currentIndex = currentImageIndexNike;
+    } else if (imageId.includes('airjordanlow')) {
+        images = airjordanlow[currentColorIndexairjordanlow];
+        currentIndex = currentImageIndexairjordanlow;
+    } else if (imageId.includes('airjordanhighog')) {
+        images = airjordanhighog[currentColorIndexairjordanhighog];
+        currentIndex = currentImageIndexairjordanhighog;
     }
 
-    // Lógica de cambio de imagen
+    // Cambiar el índice dependiendo de la dirección
     if (direction === 'prev') {
-        if (imageId.includes('hombre')) {
-            currentImageIndexHombre = (currentImageIndexHombre - 1 + images.length) % images.length;
-        } else if (imageId.includes('mujer')) {
-            currentImageIndexMujer = (currentImageIndexMujer - 1 + images.length) % images.length;
-        } else if (imageId.includes('nike')) {
-            currentImageIndexNike = (currentImageIndexNike - 1 + images.length) % images.length;
-        }
+        currentIndex = (currentIndex - 1 + images.length) % images.length;
     } else if (direction === 'next') {
-        if (imageId.includes('hombre')) {
-            currentImageIndexHombre = (currentImageIndexHombre + 1) % images.length;
-        } else if (imageId.includes('mujer')) {
-            currentImageIndexMujer = (currentImageIndexMujer + 1) % images.length;
-        } else if (imageId.includes('nike')) {
-            currentImageIndexNike = (currentImageIndexNike + 1) % images.length;
-        }
+        currentIndex = (currentIndex + 1) % images.length;
     }
 
-    // Actualizar la imagen
-    imgElement.src = images[imageId.includes('hombre') ? currentImageIndexHombre : imageId.includes('mujer') ? currentImageIndexMujer : currentImageIndexNike];
+    // Actualizar la imagen con el nuevo índice
+    imgElement.src = images[currentIndex];
+
+    // Guardar el nuevo índice según el tipo de imagen
+    if (imageId.includes('hombre')) {
+        currentImageIndexHombre = currentIndex;
+    } else if (imageId.includes('mujer')) {
+        currentImageIndexMujer = currentIndex;
+    } else if (imageId.includes('nike')) {
+        currentImageIndexNike = currentIndex;
+    } else if (imageId.includes('airjordanlow')) {
+        currentImageIndexairjordanlow = currentIndex;
+    } else if (imageId.includes('airjordanhighog')) {
+        currentImageIndexairjordanhighog = currentIndex;
+    }
 }
+
 
 function changeShoeColor(colorIndex, imageId) {
     if (imageId.includes('hombre')) {
@@ -113,6 +226,12 @@ function changeShoeColor(colorIndex, imageId) {
     } else if (imageId.includes('nike')) {
         currentColorIndexNike = colorIndex;
         currentImageIndexNike = 0;
+    } else if (imageId.includes('airjordanlow')) {
+        currentColorIndexairjordanlow = colorIndex;
+        currentImageIndexairjordanlow = 0;
+    } else if (imageId.includes('airjordanhighog')) {
+        currentColorIndexairjordanhighog = colorIndex;
+        currentImageIndexairjordanhighog= 0;
     }
 
     const imgElement = document.getElementById(imageId);
@@ -123,92 +242,34 @@ function changeShoeColor(colorIndex, imageId) {
     } else if (imageId.includes('mujer')) {
         images = shoeairforce[currentColorIndexMujer];
     } else if (imageId.includes('nike')) {
-        images = shoeNike[currentColorIndexNike];
+        images = freemetconsix[currentColorIndexNike];
+    } else if (imageId.includes('airjordanlow')) {
+        images = airjordanlow[currentColorIndexairjordanlow];
+    } else if (imageId.includes('airjordanhighog')) {
+        images = airjordanhighog[currentColorIndexairjordanhighog];
     }
 
     imgElement.src = images[0];
 }
+
+
+
 
 window.onload = function() {
     const imgElementHombre = document.getElementById('card-image-1-hombre');
     const imgElementMujer = document.getElementById('card-image-2-mujer');
     const imgElementNike = document.getElementById('card-image-3-nike');
+    const imgElementairjordanlow = document.getElementById('card-image-4-airjordanlow');
+    const imgElementairjordanhighog = document.getElementById('card-image-5-airjordanhighog');
 
     imgElementHombre.src = shoeColors[currentColorIndexHombre][currentImageIndexHombre]; 
     imgElementMujer.src = shoeairforce[currentColorIndexMujer][currentImageIndexMujer]; 
-    imgElementNike.src = shoeNike[currentColorIndexNike][currentImageIndexNike]; 
+    imgElementNike.src = freemetconsix[currentColorIndexNike][currentImageIndexNike]; 
+    imgElementairjordanlow.src = airjordanlow[currentColorIndexairjordanlow][currentImageIndexairjordanlow]; 
+    imgElementairjordanhighog.src = airjordanhighog[currentColorIndexairjordanhighog][currentImageIndexairjordanhighog]; 
 };
 
 
-
-
-
-/* mantener el cambio de los zapatos al recargar la pagina 
-
-// Función para establecer cookies
-function setCookie(name, value, days) {
-    const date = new Date();
-    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000)); // días
-    document.cookie = `${name}=${value};expires=${date.toUTCString()};path=/`;
-}
-
-// Función para obtener el valor de una cookie
-function getCookie(name) {
-    const decodedCookies = decodeURIComponent(document.cookie);
-    const cookiesArray = decodedCookies.split(';');
-    for (let cookie of cookiesArray) {
-        cookie = cookie.trim();
-        if (cookie.startsWith(name + '=')) {
-            return cookie.substring(name.length + 1);
-        }
-    }
-    return "";
-}
-
-// Guardar la selección de color o imagen en cookies
-function saveSelection(colorIndex, imageId) {
-    const selectionKey = imageId.includes('hombre') ? 'colorHombre' : 'colorMujer';
-    setCookie(selectionKey, colorIndex, 30); // Guardar cookie por 30 días
-}
-
-// Leer las cookies al cargar la página y aplicar la selección guardada
-window.onload = function() {
-    const savedColorHombre = getCookie('colorHombre');
-    const savedColorMujer = getCookie('colorMujer');
-    
-    if (savedColorHombre) {
-        currentColorIndexHombre = parseInt(savedColorHombre);
-        changeShoeColor(currentColorIndexHombre, 'card-image-1-hombre');
-    }
-    if (savedColorMujer) {
-        currentColorIndexMujer = parseInt(savedColorMujer);
-        changeShoeColor(currentColorIndexMujer, 'card-image-2-mujer');
-    }
-
-    const imgElementHombre = document.getElementById('card-image-1-hombre');
-    const imgElementMujer = document.getElementById('card-image-2-mujer');
-    imgElementHombre.src = shoeColors[currentColorIndexHombre][currentImageIndexHombre];
-    imgElementMujer.src = shoeairforce[currentColorIndexMujer][currentImageIndexMujer];
-};
-
-// Llamada a saveSelection cuando un usuario cambia el color
-function changeShoeColor(colorIndex, imageId) {
-    if (imageId.includes('hombre')) {
-        currentColorIndexHombre = colorIndex;
-        currentImageIndexHombre = 0;
-    } else {
-        currentColorIndexMujer = colorIndex;
-        currentImageIndexMujer = 0;
-    }
-
-    const imgElement = document.getElementById(imageId);
-    const images = imageId.includes('hombre') ? shoeColors[currentColorIndexHombre] : shoeairforce[currentColorIndexMujer];
-    imgElement.src = images[0];
-
-    // Guardar la selección en cookies
-    saveSelection(colorIndex, imageId);
-}
-*/
 
 
 
