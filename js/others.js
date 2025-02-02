411b8d6a4b0374cf3bac4c462ef4fb5ddd06dc2d
Jhoan Sebastian <143830061+JhoanSebasB@users.noreply.github.com>
2025-02-01 21:27:25 -0500

    Update others.js

--- js/others.js
+++ js/others.js
@@ -51,61 +51,32 @@ if (inicioIcono) { // Verifica si el elemento existe
 
 /* mensaje cuando el cursor esta apagado */
 function showMessage(cardId) {
-    let message = '';
-    let selectedSize, selectedColor, messageElement;
+    const sizes = [selectedSize1, selectedSize2, selectedSize3, selectedSize4, selectedSize5];
+    const colors = [selectedColor1, selectedColor2, selectedColor3, selectedColor4, selectedColor5];
+    const messageElement = document.getElementById(`shoe-details-${cardId}`);
 
-    // Asignar las variables correspondientes según el cardId
-    if (cardId === 1) {
-        selectedSize = selectedSize1;
-        selectedColor = selectedColor1;
-        messageElement = document.getElementById('shoe-details-1');
-    } else if (cardId === 2) {
-        selectedSize = selectedSize2;
-        selectedColor = selectedColor2;
-        messageElement = document.getElementById('shoe-details-2');
-    } else if (cardId === 3) {
-        selectedSize = selectedSize3;
-        selectedColor = selectedColor3;
-        messageElement = document.getElementById('shoe-details-3');
-    } else if (cardId === 4) {
-        selectedSize = selectedSize4;
-        selectedColor = selectedColor4;
-        messageElement = document.getElementById('shoe-details-4');
-    } else if (cardId === 5) {
-        selectedSize = selectedSize5;
-        selectedColor = selectedColor5;
-        messageElement = document.getElementById('shoe-details-5');
-    }
-
-    // Verificar si no se ha seleccionado talla o color
-    if (!selectedSize || !selectedColor) {
-        message = 'Por favor, selecciona la talla y el color antes de comprar.';
-    }
-
-    // Mostrar u ocultar el mensaje
-    if (message) {
-        messageElement.textContent = message;
-        messageElement.style.display = 'block';
-        messageElement.style.position = 'fixed';
-        messageElement.style.color = '#fff';
-        messageElement.style.backgroundColor = '#00aae4';
-        messageElement.style.padding = '10px';
-        messageElement.style.borderRadius = '5px';
-        
+    if (!sizes[cardId - 1] || !colors[cardId - 1]) {
+        messageElement.textContent = translations[currentLang]?.selectSizeColor || "Selecciona talla y color.";
+        messageElement.style.cssText = `
+            display: block;
+            position: fixed;
+            color: #fff;
+            background-color: #00aae4;
+            padding: 10px;
+            border-radius: 5px;
+        `;
 
         const button = document.getElementById(`save-button-${cardId}`);
-        const buttonRect = button.getBoundingClientRect();
-
-        messageElement.style.top = `${buttonRect.top - 40}px`;
-        messageElement.style.left = `${buttonRect.left}px`;
+        const { top, left } = button.getBoundingClientRect();
+        messageElement.style.top = `${top - 40}px`;
+        messageElement.style.left = `${left}px`;
     } else {
         messageElement.style.display = 'none';
     }
 }
 
 function hideMessage(cardId) {
-    const messageElement = document.getElementById(`shoe-details-${cardId}`);
-    messageElement.style.display = 'none'; // Ocultar el mensaje
+    document.getElementById(`shoe-details-${cardId}`).style.display = 'none';
 }
 
 
@@ -128,21 +99,32 @@ function hideMessage(cardId) {
 
         let selectedSize3 = null;
         let selectedColor3 = null;
-        let selectedShoeName3 = null;
-        let selectedShoePrice3 = null;
+        let selectedShoeName3 = "";
+        let selectedShoePrice3 = "";
 
         let selectedSize4 = null;
         let selectedColor4 = null;
-        let selectedShoeName4 = null;
-        let selectedShoePrice4 = null;
+        let selectedShoeName4 = "";
+        let selectedShoePrice4 = "";
 
         let selectedSize5 = null;
         let selectedColor5 = null;
-        let selectedShoeName5 = null;
-        let selectedShoePrice5 = null;
+        let selectedShoeName5 = "";
+        let selectedShoePrice5 = "";
     
        
         function selectSize(size, name, price, cardId) {
+            // Seleccionar los botones de tamaño dentro de la tarjeta correspondiente usando cardId
+            const sizeButtons = document.querySelectorAll(`#size-buttons-${cardId} .size-btn`);
+            
+            // Desmarcar todos los botones de tamaño dentro de esa tarjeta
+            sizeButtons.forEach(button => button.classList.remove('active'));
+        
+            // Marcar el botón de tamaño seleccionado
+            const selectedButton = [...sizeButtons].find(button => button.innerText === size);
+            if (selectedButton) selectedButton.classList.add('active');
+        
+            // Guardar los datos según el cardId
             if (cardId === 'card-image-2-mujer') {
                 selectedSize1 = size;
                 selectedShoeName1 = name;
@@ -170,9 +152,21 @@ function hideMessage(cardId) {
                 checkSelection(5);
             }
         }
-    
-    
+        
         function selectColor(color, name, price, cardId) {
+            // Seleccionar todos los botones de color dentro de la tarjeta correspondiente usando cardId
+            const colorButtons = document.querySelectorAll(`#color-buttons-${cardId} .color-btn`);
+            
+            // Desmarcar todos los botones de color dentro de esa tarjeta
+            colorButtons.forEach(button => button.classList.remove('active'));
+        
+            // Encontrar el botón correspondiente al color seleccionado
+            const selectedButton = [...colorButtons].find(button => button.classList.contains(`color-btn-${color.toLowerCase()}`));
+            
+            // Agregar la clase 'active' al botón seleccionado
+            if (selectedButton) selectedButton.classList.add('active');
+        
+            // Guardar los datos según el cardId
             if (cardId === 'card-image-2-mujer') {
                 selectedColor1 = color;
                 selectedShoeName1 = name;
@@ -200,148 +194,44 @@ function hideMessage(cardId) {
                 checkSelection(5);
             }
         }
-    
+        
+        
+        
         
         function checkSelection(cardId) {
-            let shoeDetailsElement;
-            if (cardId === 1) {
-                shoeDetailsElement = document.getElementById('shoe-details-1');
-            } else if (cardId === 2) {
-                shoeDetailsElement = document.getElementById('shoe-details-2');
-            } else if (cardId === 3) {
-                shoeDetailsElement = document.getElementById('shoe-details-3');
-            } else if (cardId === 4) {
-                shoeDetailsElement = document.getElementById('shoe-details-4');
-            } else if (cardId === 5) {
-                shoeDetailsElement = document.getElementById('shoe-details-5');
-            }
+            const shoeDetailsElement = document.getElementById(`shoe-details-${cardId}`);
+
         
             // Verificar si el elemento existe
             if (shoeDetailsElement) {
-                if (cardId === 1) {
-                    if (selectedSize1 && selectedColor1) {
-                        document.getElementById('save-button-1').disabled = false;
-                        document.getElementById('save-carrito-1').disabled = false;
-                        shoeDetailsElement.textContent = '';
-                    } else {
-                        document.getElementById('save-button-1').disabled = true;
-                        document.getElementById('save-carrito-1').disabled = true;
-                        shoeDetailsElement.textContent = '';
-                    }
-                } else if (cardId === 2) {
-                    if (selectedSize2 && selectedColor2) {
-                        document.getElementById('save-button-2').disabled = false;
-                        document.getElementById('save-carrito-2').disabled = false;
-                        shoeDetailsElement.textContent = '';
-                    } else {
-                        document.getElementById('save-button-2').disabled = true;
-                        document.getElementById('save-carrito-2').disabled = true;
-                        shoeDetailsElement.textContent = '';
-                    }
-                } else if (cardId === 3) {
-                    if (selectedSize3 && selectedColor3) {
-                        document.getElementById('save-button-3').disabled = false;
-                        document.getElementById('save-carrito-3').disabled = false;
-                        shoeDetailsElement.textContent = '';
-                    } else {
-                        document.getElementById('save-button-3').disabled = true;
-                        document.getElementById('save-carrito-3').disabled = true;
-                        shoeDetailsElement.textContent = '';
-                    }
-                } else if (cardId === 3) {
-                    if (selectedSize3 && selectedColor3) {
-                        document.getElementById('save-button-3').disabled = false;
-                        document.getElementById('save-carrito-3').disabled = false;
-                        shoeDetailsElement.textContent = '';
-                    } else {
-                        document.getElementById('save-button-3').disabled = true;
-                        document.getElementById('save-carrito-3').disabled = true;
-                        shoeDetailsElement.textContent = '';
-                    }
-                } else if (cardId === 3) {
-                    if (selectedSize3 && selectedColor3) {
-                        document.getElementById('save-button-3').disabled = false;
-                        document.getElementById('save-carrito-3').disabled = false;
-                        shoeDetailsElement.textContent = '';
-                    } else {
-                        document.getElementById('save-button-3').disabled = true;
-                        document.getElementById('save-carrito-3').disabled = true;
-                        shoeDetailsElement.textContent = '';
-                    }
-                } else if (cardId === 3) {
-                    if (selectedSize3 && selectedColor3) {
-                        document.getElementById('save-button-3').disabled = false;
-                        document.getElementById('save-carrito-3').disabled = false;
-                        shoeDetailsElement.textContent = '';
-                    } else {
-                        document.getElementById('save-button-3').disabled = true;
-                        document.getElementById('save-carrito-3').disabled = true;
-                        shoeDetailsElement.textContent = '';
-                    }
-                } else if (cardId === 4) {
-                    if (selectedSize4 && selectedColor4) {
-                        document.getElementById('save-button-4').disabled = false;
-                        document.getElementById('save-carrito-4').disabled = false;
-                        shoeDetailsElement.textContent = '';
-                    } else {
-                        document.getElementById('save-button-4').disabled = true;
-                        document.getElementById('save-carrito-4').disabled = true;
-                        shoeDetailsElement.textContent = '';
-                    }
-                } else if (cardId === 5) {
-                    if (selectedSize5 && selectedColor5) {
-                        document.getElementById('save-button-5').disabled = false;
-                        document.getElementById('save-carrito-5').disabled = false;
-                        shoeDetailsElement.textContent = '';
-                    } else {
-                        document.getElementById('save-button-5').disabled = true;
-                        document.getElementById('save-carrito-5').disabled = true;
-                        shoeDetailsElement.textContent = '';
-                    }
+                const cardIds = [1, 2, 3, 4, 5];
+                const selectedSizes = [selectedSize1, selectedSize2, selectedSize3, selectedSize4, selectedSize5];
+                const selectedColors = [selectedColor1, selectedColor2, selectedColor3, selectedColor4, selectedColor5];
+
+                if (cardIds.includes(cardId)) {
+                    const index = cardIds.indexOf(cardId);
+                    const isEnabled = selectedSizes[index] && selectedColors[index];
+
+                    document.getElementById(`save-button-${cardId}`).disabled = !isEnabled;
+                    document.getElementById(`save-carrito-${cardId}`).disabled = !isEnabled;
+                    shoeDetailsElement.textContent = '';
                 }
+
             } 
         }
         
     
         // Función para guardar la información y mostrarla
-        function saveShoeDetails(cardId) {
-    let shoeData;
-    if (cardId === 'card-image-2-mujer') {
-        shoeData = {
-            size: selectedSize1,
-            color: selectedColor1,
-            name: selectedShoeName1,
-            price: selectedShoePrice1
-        };
-    } else if (cardId === 'card-image-1-hombre') {
-        shoeData = {
-            size: selectedSize2,
-            color: selectedColor2,
-            name: selectedShoeName2,
-            price: selectedShoePrice2
-        };
-    } else if (cardId === 'card-image-3-nike') {
-        shoeData = {
-            size: selectedSize3,
-            color: selectedColor3,
-            name: selectedShoeName3,
-            price: selectedShoePrice3
-        };
-    } else if (cardId === 'card-image-4-airjordanlow') {
-        shoeData = {
-            size: selectedSize4,
-            color: selectedColor4,
-            name: selectedShoeName4,
-            price: selectedShoePrice4
-        };
-    } else if (cardId === 'card-image-5-airjordanhighog') {
-        shoeData = {
-            size: selectedSize5,
-            color: selectedColor5,
-            name: selectedShoeName5,
-            price: selectedShoePrice5
-        };
-    }
+       function saveShoeDetails(cardId) {
+    const shoeDataMap = {
+        'card-image-2-mujer': { size: selectedSize1, color: selectedColor1, name: selectedShoeName1, price: selectedShoePrice1 },
+        'card-image-1-hombre': { size: selectedSize2, color: selectedColor2, name: selectedShoeName2, price: selectedShoePrice2 },
+        'card-image-3-nike': { size: selectedSize3, color: selectedColor3, name: selectedShoeName3, price: selectedShoePrice3 },
+        'card-image-4-airjordanlow': { size: selectedSize4, color: selectedColor4, name: selectedShoeName4, price: selectedShoePrice4 },
+        'card-image-5-airjordanhighog': { size: selectedSize5, color: selectedColor5, name: selectedShoeName5, price: selectedShoePrice5 }
+    };
+
+    const shoeData = shoeDataMap[cardId];
 
     // Verificar si shoeData fue definido
     if (!shoeData) {
@@ -353,26 +243,24 @@ function hideMessage(cardId) {
     console.log("Información del zapato guardada:", shoeData);
 
     try {
-    const message = `
-    ¡Estoy interesado en los siguientes zapatos! 👟
-        Nombre: ${shoeData.name}
-        Talla: ${shoeData.size}
-        Color: ${shoeData.color}
-        Precio: ${shoeData.price}
-    `;
-    const phoneNumber = "+573209504702";  
-    const encodedMessage = encodeURIComponent(message);
-    const url = `https://wa.me/${phoneNumber}?text=${encodedMessage}`;
+        const message = `
+        ¡Estoy interesado en los siguientes zapatos! 👟
+            Nombre: ${shoeData.name}
+            Talla: ${shoeData.size}
+            Color: ${shoeData.color}
+            Precio: ${shoeData.price}
+        `;
+        const phoneNumber = "+573209504702";  
+        const encodedMessage = encodeURIComponent(message);
+        const url = `https://wa.me/${phoneNumber}?text=${encodedMessage}`;
 
-    console.log("Mensaje generado:", message);
-    console.log("URL de WhatsApp:", url);
+        console.log("Mensaje generado:", message);
+        console.log("URL de WhatsApp:", url);
 
-    window.open(url, '_blank');
-} catch (error) {
-    console.error("Error al generar el enlace de WhatsApp:", error);
+        window.open(url, '_blank');
+    } catch (error) {
+        console.error("Error al generar el enlace de WhatsApp:", error);
+    }
 }
-
-}
-
 
  
